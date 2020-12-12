2d5c6fb7f08192d1a88a14c5ed254c3f36a2c5e5
shivani0852 <sbhati@bestpeers.com>
2020-12-12 16:09:04 +0530

    Added status total_price and total_price definations in Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,14 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :line_items
+  enum status: [:cart, :payment, :deliver, :shipped, :cancelled]
+
+
+  def total_price
+    (line_items.price * line_items.quantity).sum
+  end
+
+  def total_quantity
+    line_items.quantity
+  end
 end

e6ed5ad808844c0ee1b6ff4a21a6cfab5351cda1
shivani0852 <sbhati@bestpeers.com>
2020-12-15 15:38:25 +0530

    Makes price method in line item model

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,20 @@ class LineItem < ApplicationRecord
   belongs_to :user
   belongs_to :product
   belongs_to :order
+
+  before_save :set_price
+
+  def price
+    if persisted?
+      self[:price]
+    else
+      product.price
+    end
+  end
+
+  private
+
+    def set_price
+      self[:price] = price
+    end
 end

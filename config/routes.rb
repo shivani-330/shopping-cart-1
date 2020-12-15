b042a16bbe1a80cbe57db9388ede7a53a7577afa
shivani0852 <sbhati@bestpeers.com>
2020-12-15 16:43:35 +0530

    Makes nested routes of line_items under orders

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
+  resources :orders do
+    resources :line_items, except: [:index]
+  end
+  resources :line_items, only: [:index]
 end

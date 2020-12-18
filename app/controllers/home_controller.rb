4d1762df38ab7fcf9c2cd3f3425e2494547304b8
shivani0852 <sbhati@bestpeers.com>
2020-12-18 21:16:20 +0530

    Added pagination in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @products = Product.all
+    # @products = Product.all
+    @products = Product.paginate(page: params[:page], per_page: 2).order('created_at DESC')
   end
 end

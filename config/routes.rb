58b7d4977318ccedf4262692b574d089f2e7fd13
shivani0852 <sbhati@bestpeers.com>
2020-12-18 17:35:14 +0530

    Routes Chnges

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
   resources :orders do
-    resources :line_items, except: [:index]
+    resources :line_items
   end
-  resources :line_items, only: [:index]
+  resource :carts, only: [:show]
 end

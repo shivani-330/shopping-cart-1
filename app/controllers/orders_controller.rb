57480ca58dfd96f3ec8f8061f735bbc04d883305
shivani0852 <sbhati@bestpeers.com>
2020-12-21 21:20:15 +0530

    Do some changes in place order in orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,11 +11,10 @@ class OrdersController < ApplicationController
   end
 
   def place_order
-    @order = Order.new(order_params) 
-    @total_quantity = LineItem.where(user_id: current_user.id).sum('quantity')
-    @total_price =  LineItem.where(user_id: current_user.id).sum('price')
-    @order = current_user.latest_order.update(total_price: @total_price, 
-      status: 'payment',total_quantity: @total_quantity, payment_mode: @order.payment_mode)
+    @new_order = Order.new(order_params) 
+    @order = current_user.latest_order
+    @update_order = current_user.latest_order.update(total_price: @order.total_price, 
+      status: 'payment', total_quantity: @order.total_quantity, payment_mode: @order.payment_mode)
     redirect_to orders_path
   end
   
